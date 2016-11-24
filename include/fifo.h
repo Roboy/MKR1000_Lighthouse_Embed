b36d19fdf7f43c6e87cfd8ee8c02d06f79db5669
SNBasti <snbasti94@gmail.com>
2016-11-24 10:51:47 +0100

    forget to add fifo header

---
+++ include/fifo.h
@@ -0,0 +1,36 @@
+#ifndef FIFO_H
+#define FIFO_H
+
+#include "board.h"
+
+/**
+ * see mikrocontroller forum tutorial 
+ * */
+typedef struct _FIFO128t{
+    uint8_t     mRead;
+    uint8_t     mWrite;
+    uint16_t    mBuffer[128]; 
+}FIFO128t; 
+
+//FIFO Operations are implemented as Preprocessor Functions since the FIFO is used in several IRQs an things need to get a bit faster
+#define FIFO_init(fifo)                 { fifo.mRead = 0; fifo.mWrite = 0;}
+
+#define FIFO_available(fifo)            ( fifo.mRead != fifo.mWrite )
+
+#define FIFO_read(fifo, size)           (                                           \
+        (FIFO_available(fifo)) ?                                                    \
+        fifo.mBuffer[(fifo.mRead = ((fifo.mRead + 1) & (size -1)))] : 0             \
+        ) 
+
+#define FIFO_write(fifo, data, size)    {                                           \
+    uint8_t temp = ((fifo.mWrite +1 )& (size -1));                                  \
+    if(temp != fifo.mRead) {                                                        \
+        fifo.mBuffer[temp] = data;                                                  \
+        fifo.mWrite = temp;                                                         \
+    }                                                                               \
+}
+
+#define FIFO128_read(fifo)              FIFO_read(fifo, 128)
+#define FIFO128_write(fifo, data)       FIFO_write(fifo, data, 128)
+
+#endif // FIFO_H
